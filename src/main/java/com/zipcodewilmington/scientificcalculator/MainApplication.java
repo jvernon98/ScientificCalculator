53cf28b600e936821e4aaf38efac736a12841ebe
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-04 14:20:45 -0500

    I've added the buttons and most of the functionalities of the buttons so far

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 /**
  * Created by leon on 2/9/18.
  */
@@ -13,5 +16,116 @@ public class MainApplication {
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
+
+        Scanner scanner = new
+                Scanner(System.in);
+        boolean exit = false;
+        double currentValue = 0.0;
+        while (!exit) {
+            System.out.println("Select an option:");
+                System.out.println("Current value: ");
+                System.out.println("1. Addition");
+                System.out.println("2. Subtraction");
+                System.out.println("3. Multiplication");
+                System.out.println("4. Division");
+                System.out.println("5. Sine");
+                System.out.println("6. Cosine");
+                System.out.println("7. Tangent");
+                System.out.println("8. Clear Display");
+                System.out.println("9. Change number on Display");
+                System.out.println("0. Exit");
+
+                int choice =scanner.nextInt();
+                switch (choice) {
+
+                    case 1:
+                        System.out.println("Enter the number to add");
+                        double num1 = scanner.nextDouble();
+                        currentValue += num1;
+                        System.out.println("Result:" + currentValue);
+                        break;
+
+                    case 2:
+                        System.out.println("Enter the number to subtract: ");
+                        num1 = scanner.nextDouble();
+                        currentValue -= num1;
+                        System.out.println("Result:" + currentValue);
+                        break;
+
+                    case 3:
+                        System.out.println("Enter the number to multiply: ");
+                        num1 = scanner.nextDouble();
+                        currentValue *= num1;
+                        System.out.println("Result:" + currentValue);
+                        break;
+
+                    case 4:
+                        System.out.println("Enter the number to divide: ");
+                        num1 = scanner.nextDouble();
+                        if (num1 == 0.0){
+                        System.out.println("Error: Division by Zero");
+                        currentValue = Double.NaN;
+                    } else {
+                            currentValue /=num1;
+                            System.out.println("Result: " + currentValue);
+                    }
+                        break;
+                    case 5:
+                        System.out.println("Enter the angle in degrees: ");
+                        double angle =
+                                scanner.nextDouble();
+                                double sinValue =
+                                        Math.sin(Math.toRadians(angle));
+                                        if (Double.isNaN(sinValue)) {
+                                            System.out.println("Error: Invalid Input");
+                                            currentValue = Double.NaN;
+                                        } else {
+                                            currentValue = sinValue;
+                                            System.out.println("Result: " + currentValue);
+                                        }
+                                            break;
+                    case 6:
+                        System.out.println("Enter the angle in degrees: ");
+                        angle = scanner.nextDouble();
+                        double cosValue =
+                                Math.cos(Math.toRadians(angle));
+                                if (Double.isNaN(cosValue)) {
+                                    System.out.println("Error: Invalid Input");
+                                    currentValue = Double.NaN;
+                                } else {
+                                    currentValue = cosValue;
+                                    System.out.println("Result: " + currentValue);
+                                } break;
+                    case 7:
+                        System.out.println("Enter the angle in degrees: ");
+                        angle = scanner.nextDouble();
+                        double tanValue =
+                                Math.tan(Math.toRadians(angle));
+                                if (Double.isNaN(tanValue)) {
+                                    System.out.println("Error: Invalid Input");
+                                    currentValue = Double.NaN;
+                                } else {
+                                    currentValue = tanValue;
+                                    System.out.println("Result: " + currentValue);
+                                } break;
+                        currentValue += num1;
+                        System.out.println("Result:" + currentValue);
+                        break;
+                    case 1:
+                        System.out.println("Enter the number to add");
+                        double num1 = scanner.nextDouble();
+                        currentValue += num1;
+                        System.out.println("Result:" + currentValue);
+                        break;
+                    case 1:
+                        System.out.println("Enter the number to add");
+                        double num1 = scanner.nextDouble();
+                        currentValue += num1;
+                        System.out.println("Result:" + currentValue);
+                        break;
+
+
+                }
+        }
     }
 }
